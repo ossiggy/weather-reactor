c4ec53842e2cc72515dcd992f7fdb49e279ede77
Ossiggy <mrossig201@gmail.com>
2017-12-15 16:54:33 -0500

    updated html to remove search by name. will only do search by zip for now.  Also, added weather conditions id for use with animated effects later

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-$('#search-by-name').on('submit', searchByCity);
+// $('#search-by-name').on('submit', searchByCity); may add international capacity later
 $('#search-by-zip').on('submit', searchByZip);
 
-function searchByCity(event) {
-  event.preventDefault();
-  const cityName = $('#city-name-input').val().replace(/ +, /g, "+");
-  returnCityWeatherData(cityName)
-}
+// function searchByCity(event) {
+//   event.preventDefault();
+//   const cityName = $('#city-name-input').val().replace(/ +, /g, "+");
+//   returnCityWeatherData(cityName)
+// }
 
-function returnCityWeatherData(city) {
-  const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&type=accurate&APPID=99689a8e1e9e9c4a36e72d7867397088"
-  $.getJSON(url, function(response) {
-    filter(response);
-  });
-};
+// function returnCityWeatherData(city) {
+//   const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&type=accurate&APPID=99689a8e1e9e9c4a36e72d7867397088"
+//   $.getJSON(url, function(response) {
+//     filter(response);
+//   });
+// };
 
 function searchByZip(event) {
   event.preventDefault();
@@ -23,21 +23,28 @@ function searchByZip(event) {
 function returnZipWeatherData(zip) {
   const url = "https://api.openweathermap.org/data/2.5/weather?zip="+zip+"&units=imperial&type=accurate&APPID=99689a8e1e9e9c4a36e72d7867397088"
   $.getJSON(url, function(response) {
+    console.log(response)
     filter(response);
   });
 };
 
 function filter(object) {
-  const {clouds, coord, main, sys, weather, wind} = object
+  const {clouds, coord, name, main, sys, weather, wind} = object
   const importantData = {
+    'name': name,
     'clouds': clouds,
     'coord': coord,
     'main': main,
     'sys': sys,
-    'weather': weather,
+    'weather': weather.map(function(condition){
+      return " "+condition.description
+    }),
+    'weather_id': weather.map(function(condition){
+      return condition.id
+    }),
     'wind': wind
   }
-  console.log(importantData)
+  // console.log(importantData)
   updateDom(importantData)
 }
 
@@ -50,18 +57,31 @@ function updateDom(weather){
   const sunrise = convertTimestamp(weather.sys.sunrise);
   const sunset = convertTimestamp(weather.sys.sunset);
 
+  console.log(weather.weather_id)
+
+  let windDir
+  if(weather.wind.deg){
+    windDir = calculateDirection(weather.wind.deg);
+  }
+  else{
+    windDir = '';
+  }
+
   $('#weather-report').append(
-    `<ul>
-      <li>Precipitation: ${weather.clouds.all}%</li>
-      <li>Temp: ${temp}&deg;F</li>
-      <li>High: ${high}&deg;F</li>
-      <li>Low: ${low}&deg;F</li>
-      <li>Humidity: ${weather.main.humidity}%</li>
-      <li>Longitute: ${weather.coord.lon}</li>
-      <li>Latitude: ${weather.coord.lat}</li>
-      <li>Sunrise: ${sunrise}</li>
-      <li>Sunset: ${sunset}</li>
-    </ul>
+    `<h2>${weather.name}</h2>
+      <ul>
+        <li>${weather.weather}</li>
+        <li>Precipitation: ${weather.clouds.all}%</li>
+        <li>Temp: ${temp}&deg;F</li>
+        <li>High: ${high}&deg;F</li>
+        <li>Low: ${low}&deg;F</li>
+        <li>Humidity: ${weather.main.humidity}%</li>
+        <li>Wind Speed: ${weather.wind.speed} mph ${windDir}</li>
+        <li>Longitute: ${weather.coord.lon}</li>
+        <li>Latitude: ${weather.coord.lat}</li>
+        <li>Sunrise: ${sunrise}</li>
+        <li>Sunset: ${sunset}</li>
+      </ul>
     `
   )
 }
@@ -87,4 +107,55 @@ function convertTimestamp(timestamp) {
 	time = h + ':' + min + ' ' + ampm;
 		
 	return time;
+}
+
+function calculateDirection(d){
+  if(d===0||d===360){
+    return 'N'
+  }
+  if(d>0&&d<45){
+    return 'N/NE'
+  }
+  if(d===45){
+    return 'NE'
+  }
+  if(d>45&&d<90){
+    return 'E/NE'
+  }
+  if(d===90){
+    return 'E'
+  }
+  if(d>90&&d<135){
+    return 'E/SE'
+  }
+  if(d===135){
+    return 'SE'
+  }
+  if(d>135&&d<180){
+    return 'S/SE'
+  }
+  if(d===180){
+    return 'S'
+  }
+  if(d>180&&d<225){
+    return 'S/SW'
+  }
+  if(d===225){
+    return 'SW'
+  }
+  if(d>225&&d<270){
+    return 'W/SW'
+  }
+  if(d===270){
+    return 'W'
+  }
+  if(d>270&&d<315){
+    return 'W/NW'
+  }
+  if(d===315){
+    return 'W'
+  }
+  if(d>315&&d<360){
+    return 'N/NW'
+  }
 }
